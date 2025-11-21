68c179ab0db424e25818f5fd228e86035ae51631
Gabriel Silva <gabrielaugusto.ifpr2024@gmail.com>
2025-11-21 11:59:36 -0300

    Add files via upload

---
+++ moda-da-mulher-main/footer.php
@@ -0,0 +1,55 @@
+<footer class="bg-cor-secundaria text-white">
+          <div class="container py-5">
+
+              <div class="row">
+
+                <div class="col-md-3">
+                  <h5 class="text-primary">Atendimento</h5>
+                  <ul>
+                    <li>(11) 99999-9999</li>
+                    <li>email@gmail.com</li>
+                    <li>segunda a sexta das 9h as 20h</li>
+                  </ul>
+                </div>
+
+                <div class="col-md-3">
+                <h5 class="text-primary">Acesso rápido</h5>
+
+                  <ul>
+                    <li>(11) 99999-9999</li>
+                    <li>email@gmail.com</li>
+                    <li>segunda a sexta das 9h as 20h</li>
+                  </ul>
+                </div>
+
+                <div class="col-md-3">
+                  <h5 class="text-primary">Mais acessadas</h5>
+                  <ul>
+                    <li>(11) 99999-9999</li>
+                    <li>email@gmail.com</li>
+                    <li>segunda a sexta das 9h as 20h</li>
+                  </ul>
+                </div>
+
+                <div class="col-md-3 mb-5 mb-md-0">
+                  <h5 class="text-primary">Saiba mais</h5>
+                  <ul>
+                    <li>(11) 99999-9999</li>
+                    <li>email@gmail.com</li>
+                    <li>segunda a sexta das 9h as 20h</li>
+                  </ul>
+                </div>
+
+              </div>
+
+          </div>
+         </footer>
+
+      </div>
+
+    </div>
+
+  <script src="assets/scripts/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
